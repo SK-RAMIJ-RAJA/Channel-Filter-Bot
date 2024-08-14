c2076db0de8e21e03471d2e3c5f82c50b3ecbcdb
SK-RAMIJ-RAJA <akabusiness010@gmail.com>
2024-08-14 17:43:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+API_ID       = 26562024
+API_HASH     = "c105472ed5fd3ebf112c7e9b5fc44a40"
+BOT_TOKEN    = "7324348362:AAFmHs2eeUi5_kFMUhrx14jnHFGwTY5QgI0"
+SESSION      = "BQGVTegAUUznjuzEb88zs_3uhzZW8_tWMMLc0uaI8-zFsjWxoepIl5Yhw5CoBSk8Jrnfxvf9L1jB6FQkCNOdnIbH18DllioIwucu8KAU3moFvr71ZQOtuNBoNRy5vytS7PNdW_qUI3IfnvsdqViYR071sp7xhLnW8pI2DswIGMfyRWkwuqOYIjGz4vuYkO_6CICFXL2xv3G_wt1rCy3k8xxMGf-CIC78bo4NmS-bHVCS5WUhNggsC_uUl061oRVB636qP-L0PuPNuQmoCF0T8dtSvpjQzhUzvcQwH0l-ibkVu7BjVsPWn0qgAAPlCk3FTBR72dOFOs_AKhplttSzmitg_WxQAAAAGUmXnSAA"
+DATABASE_URI = "mongodb+srv://pybizy:9883364643rj@cluster0.pzmvz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+LOG_CHANNEL  = -1002162289652
+ADMIN        = 6788053458
+CHANNEL      = "@BfUMovies"
